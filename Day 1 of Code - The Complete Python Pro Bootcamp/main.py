ec7c5644db5c20ec3ecbd4db2b1e204850f646b5
deyyybid <deybid.eyron21@gmail.com>
2025-07-09 20:33:31 +0800

    Update Day 1 of Coding Python

--- Day 1 of Code - The Complete Python Pro Bootcamp/main.py
+++ Day 1 of Code - The Complete Python Pro Bootcamp/main.py
@@ -1,1 +1,33 @@
-print('Hello, World!')
+# Print Function
+print("Hello, World!")
+
+# String Manipulation
+# Possible Error: IndentationError or SyntaxError
+print("Hello, World!\nGood Bye, World!")
+print("Hello," + " " + "David!")
+
+# Improved: Using String Format
+print(f"Hello, {"David!"}")
+
+# Input Function
+# Single Liner
+print("Hello, " + input("Hello, what is your name?\nName: ") + "!")
+
+# Variables
+# Possible Error: SyntaxError, NameError, TypeError
+myPhoneNumber = "09123456789" # Don"t use int if it"s start with 0 value, Use string
+print(f"This is my Phone Number: {myPhoneNumber}")
+myPhoneNumber = "09987654321"
+print(f"This is my Phone Number: {myPhoneNumber}") # Output: 09987654321
+print(f"Length of Phone Number: {len(myPhoneNumber)}")
+age = 23
+print(f"I was born in the year 2002, and I am {age} years old now.")
+
+# Improved Code with Variables
+name = input("Hello, what is your name?\nName: ")
+print(f"Hello, {name}!")
+
+myDigits = 264890702902689430234689893469082346
+myDigitsLength = len(str(myDigits))
+print(f"The Digit \"{myDigits}\" has a total of {myDigitsLength} numbers")
+print(f'The Digit "{myDigits}" has a total of {myDigitsLength} numbers')
